9c38556fd282fdfbff0f21596702f8008a558235
Dragan Markovic <drag.markovic@gmail.com>
2021-11-26 18:04:16 +0100

    changed build config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const glob = require("glob")
+
+module.exports = {
+    entry: {
+        "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
+    },
+    output: {
+        filename: "build/static/js/bundle.min.js",
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            { test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+
+        ],
+    },
+    plugins: [new UglifyJsPlugin()],
+}
